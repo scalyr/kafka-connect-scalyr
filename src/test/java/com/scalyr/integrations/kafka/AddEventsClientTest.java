954dfc723880827cda8e9499bff1f6698b7f13e6
Oliver Hsu <oliver@scalyr.com>
2020-10-09 22:08:49 -0700

    CT-195 Send serverHost as __origServerHost

--- src/test/java/com/scalyr/integrations/kafka/AddEventsClientTest.java
+++ src/test/java/com/scalyr/integrations/kafka/AddEventsClientTest.java
@@ -78,7 +78,7 @@ public class AddEventsClientTest {
   private static final String ID = "id";
   private static final String MESSAGE = "message";
   private static final String PARSER = "parser";
-  private static final String SERVERHOST = "serverHost";
+  private static final String SERVERHOST = "__origServerHost";
   private static final String LOGFILE = "logfile";
 
   private static final int numServers = 5;
